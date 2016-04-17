35d805ec66a31bcf3916779eaf5f5166ddb005da
roopsmall <roopsmall@outlook.com>
2016-04-17 12:08:08 +0100

    add toolbox.py for activation functions

---
+++ toolbox.py
@@ -0,0 +1,17 @@
+from numpy import exp
+
+def sigmoid(x):
+	# sigmoid activation function for MLP nodes
+	return 1/(1+exp(-x));
+	
+def tanh():
+        # tanh activation function
+        pass
+
+def softmax():
+        # softmax activation function
+        pass
+
+def heaviside():
+        # heaviside activation function
+        pass
